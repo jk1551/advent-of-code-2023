13138b3d0d8e7fbd3404b2aa4f6846d9c23f70a7
Joseph King <joe.k659@gmail.com>
2023-12-02 10:20:05 -0500

    solution

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -13,21 +13,46 @@ fn main() {
     // Cheeck to make sure file is readable per Rust docs
     if let Ok(lines) = read_lines("./puzzle-input.txt") {
         // Create variable to store total
-        let total = 0;
+        let mut _total: i32 = 0;
         for line in lines {
             if let Ok(word) = line {
+                let mut _first: Option<i32> = None; 
+                let mut _last: Option<i32> = None; 
                 for letter in word.chars() {
-                    let first: i32 = 0;
-                    let last: i32 = 0;
                     // Set first number to first
-                    // As you parse the numbers, set last number to last
-                    println!("{}", letter);
-                    println!("{}", first);
-                    println!("{}", last);
+                    // As you parse the numbers if there is more than 1, set last number to last
+                    if letter.is_numeric() {
+                        if let Some(digit_value) = letter.to_digit(10) {
+                            match _first {
+                                Some(_value) => {
+                                    println!("{}", digit_value as i32);
+                                    _last = Some(digit_value as i32);
+                                }
+                                None => {
+                                    println!("{}", digit_value);
+                                    _first = Some(digit_value as i32);
+                                    _last = Some(digit_value as i32);
+                                }
+                            }
+                        }
+                    }
+                }
+                // Concat the first and last value into a string so that it is added like
+                // 2 + 2 = 22
+                // Not 2 + 2 = 4
+                let concatenated_sum = match (_first, _last) {
+                    (Some(first_str), Some(last_str)) => format!("{}{}", first_str, last_str),
+                    (Some(first_str), None) => format!("{}{}", first_str, first_str),
+                    _ => String::new(),
+                };
+        
+                if let Ok(sum) = concatenated_sum.parse::<i32>() {
+                    // convert the string back to int32 so that we can add to the total
+                    _total += sum;
+                    println!("Total: {}", _total);
                 }
             }
         }
-        return total;
     }
 }
 
